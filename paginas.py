e1f2a860e8acbab8acfa176bdb003987ccbb62cf
Víctor Hernández Piñeiro <victor.hernandez.pineiro@udc.es>
2026-01-27 18:23:30 +0100

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -58,16 +58,16 @@ def archivos():
 						if i < len(datos) - 1 and datos.loc[i+1, columnname] >= 0:
 							k_descarga += 1
 				else:
-					if current == 0 and datos.loc[i-1,"Paso"]!=f"Discharge {k_descarga}":
+					if current == 0 and datos.loc[i-1,"Current(mA)"]==0:
 						datos.loc[i, "Paso"] = "Rest"
 						#datos.loc[i, "Ciclo"] = 0
-					elif current >= 0 and datos.loc[i-1,"Paso"]==f"Discharge {k_descarga}":
+					elif current >= 0 and datos.loc[i-1,"Current(mA)"]<0:
 						datos.loc[i, "Paso"] = f"Charge {k_carga}"
 						datos.loc[i, "Ciclo"] = k_carga
 						# Si el siguiente valor cambia de signo o a cero, pasamos al siguiente ciclo
 						if i < len(datos) - 1 and datos.loc[i+1, columnname] <= 0:
 							k_carga += 1
-					elif current <= 0:
+					elif current < 0:
 						datos.loc[i, "Paso"] = f"Discharge {k_descarga}"
 						datos.loc[i, "Ciclo"] = k_descarga
 						if i < len(datos) - 1 and datos.loc[i+1, columnname] >= 0:
@@ -142,6 +142,7 @@ def archivos():
 		# st.plotly_chart(fig, use_container_width=True)
 
 				
+
 
 
 
