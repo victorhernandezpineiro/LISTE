f641ebb3928a13a9c9a3a1e2917b5f47ccefbfa2
Víctor Hernández Piñeiro <victor.hernandez.pineiro@udc.es>
2026-01-27 18:27:47 +0100

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -42,36 +42,21 @@ def archivos():
 				elif "Current(µA)" in datos.columns:
 					current = datos.loc[i, "Current(µA)"]
 					columnname = "Current(µA)"
-				if i==0:
-					if current == 0:
-						datos.loc[i, "Paso"] = "Rest"
-						#datos.loc[i, "Ciclo"] = 0
-					elif current >= 0 and datos.loc[i-1,"Paso"]!=f"Discharge {k_descarga}":
-						datos.loc[i, "Paso"] = f"Charge {k_carga}"
-						datos.loc[i, "Ciclo"] = k_carga
-						# Si el siguiente valor cambia de signo o a cero, pasamos al siguiente ciclo
-						if i < len(datos) - 1 and datos.loc[i+1, columnname] <= 0:
-							k_carga += 1
-					elif current <= 0:
-						datos.loc[i, "Paso"] = f"Discharge {k_descarga}"
-						datos.loc[i, "Ciclo"] = k_descarga
-						if i < len(datos) - 1 and datos.loc[i+1, columnname] >= 0:
-							k_descarga += 1
-				else:
-					if current == 0:
-						datos.loc[i, "Paso"] = "Rest"
-						#datos.loc[i, "Ciclo"] = 0
-					elif current >= 0 and datos.loc[i-1,"Paso"]!="Rest":
-						datos.loc[i, "Paso"] = f"Charge {k_carga}"
-						datos.loc[i, "Ciclo"] = k_carga
-						# Si el siguiente valor cambia de signo o a cero, pasamos al siguiente ciclo
-						if i < len(datos) - 1 and datos.loc[i+1, columnname] <= 0:
-							k_carga += 1
-					elif current < 0:
-						datos.loc[i, "Paso"] = f"Discharge {k_descarga}"
-						datos.loc[i, "Ciclo"] = k_descarga
-						if i < len(datos) - 1 and datos.loc[i+1, columnname] >= 0:
-							k_descarga += 1
+				if datos.loc[i,"Step Type"] == "Rest":
+					datos.loc[i, "Paso"] = "Rest"
+					#datos.loc[i, "Ciclo"] = 0
+				elif current >= 0:
+					datos.loc[i, "Paso"] = f"Charge {k_carga}"
+					datos.loc[i, "Ciclo"] = k_carga
+					# Si el siguiente valor cambia de signo o a cero, pasamos al siguiente ciclo
+					if i < len(datos) - 1 and datos.loc[i+1, columnname] <= 0:
+						k_carga += 1
+				elif current <= 0:
+					datos.loc[i, "Paso"] = f"Discharge {k_descarga}"
+					datos.loc[i, "Ciclo"] = k_descarga
+					if i < len(datos) - 1 and datos.loc[i+1, columnname] >= 0:
+						k_descarga += 1
+
 
 		st.dataframe(datos)
 
@@ -142,6 +127,7 @@ def archivos():
 		# st.plotly_chart(fig, use_container_width=True)
 
 				
+
 
 
 
