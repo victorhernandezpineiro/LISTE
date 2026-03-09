06155656b3ef121c0d3eb957ec958e5b4238b8b8
Víctor Hernández Piñeiro <victor.hernandez.pineiro@udc.es>
2026-03-09 16:25:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ if not st.session_state["authenticated"]:
         if username in USERS and USERS[username]["password"] == password:
             st.session_state["authenticated"] = True
             st.session_state["username"] = username
-            st.success(f"Bienvenido, {USERS[username]['name']}!")
-            st.session_state["pagina"] = "inicio"  # redirige a la página de inicio
+            st.success(st.session_state["pagina"] = "inicio")  # redirige a la página de inicio#f"Bienvenido, {USERS[username]['name']}!")
+            
             st.write("Bienvenido a la web interna de LISTE para tratamiento de datos de cicladores")
             
         else:
@@ -61,6 +61,7 @@ if st.session_state["authenticated"]:
         p.archivos()
     elif st.session_state["pagina"] == "comparar":
         p_cp.comparar()
+
 
 
 
